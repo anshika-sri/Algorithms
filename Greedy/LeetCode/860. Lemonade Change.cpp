1946d1600d9e00e170b13e066f6684776b8649ba
anshika-sri <35023358+anshika-sri@users.noreply.github.com>
2021-01-10 22:33:43 +0530

    Update 860. Lemonade Change.cpp

--- Greedy/LeetCode/860. Lemonade Change.cpp
+++ Greedy/LeetCode/860. Lemonade Change.cpp
@@ -1,1 +1,32 @@
+/**
+Please cjeck that it gives wrong answer at online judge
+**/
+
+
+class Solution {
+public:
+    bool lemonadeChange(vector<int> &bills) {
+        
+        int five = 0, ten = 0, twenty = 0;
+        
+        for(int i = 0; i < bills.size(); i++) {
+            if(bills[0] != 5) { return false; }
+            else if(bills[i] == 5) { five++; continue; }
+            else if(bills[i]  == 10) {
+               
+                if(five >= 1) { five--;  ten++; continue; }
+                else { return false; }
+            }
+            else if(bills[i]  == 20) {
+                
+                if(five >= 3) { five =  five - 3; twenty++; continue; }
+                else if(ten >= 1 && five >= 1) { ten--; five--; twenty++; continue; }
+                else { return false; }
+            }
+            else { return false; }
+        }
+        
+        return true;
+    }
+};
 
